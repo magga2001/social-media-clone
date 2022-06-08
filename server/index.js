910ee7bb45efe81afd84fc15a2108b0174f008e4
Switt Wattanalekhawong <switt.wattanalekhawong@kcl.ac.uk>
2022-06-08 18:12:43 +0100

    Fix server side

--- server/index.js
+++ server/index.js
@@ -6,6 +6,15 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const app = express();
-const PORT = process.env || 5000;
+
+// app.use(bodyParser.json({ limit: "30mb", extended: true }))
+// app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+// app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('APP IS RUNNING. ');
+})
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
