b2953f48224a31ec6ea1137ffc671effa8aa5a25
Switt Wattanalekhawong <switt.wattanalekhawong@kcl.ac.uk>
2022-06-09 06:08:08 +0100

    Change starter setup

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
 dotenv.config();
 const app = express();
+app.use(express.json())
 
 // app.use(bodyParser.json({ limit: "30mb", extended: true }))
 // app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
-// app.use(cors());
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('APP IS RUNNING. ');
